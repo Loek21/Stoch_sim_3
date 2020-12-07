36aec0fd28c6a355dcab2dbb6e4af6002a0b9aec
Loek21 <ljvansteijn@outlook.com>
2020-12-07 22:55:27 +0100

    update

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ import csv
 if __name__ == "__main__":
     coord_list = read_inpt('a280.tsp')
 
-    for i in range(100):
-        random.shuffle(coord_list)
-        markov, length, delta_L_list = simulated_annealing(cooling_method=geom_cool, coord_list=coord_list, initial_time=1, \
-            max_time=500, initial_temperature=200)
-        with open("geom_cool.csv", "a") as f:
-            writer=csv.writer(f, delimiter=';')
-            writer.writerow([len(length)])
-    # initial_T = T_init(delta_L_list, 200, 0.8, 0.01)
-    # print(initial_T)
+    for j in [10,100,1000,10000]:
+        for i in range(100):
+            random.shuffle(coord_list)
+            markov, length, delta_L_list = simulated_annealing(cooling_method=exp_cool, coord_list=coord_list, initial_time=1, \
+                max_time=j, initial_temperature=200)
+            with open(f"{j}.csv", "a") as f:
+                writer=csv.writer(f, delimiter=';')
+                writer.writerow([np.min(length), length[-1]])
+        # initial_T = T_init(delta_L_list, 200, 0.8, 0.01)
+        # print(initial_T)
 
-    plt.plot(np.linspace(1,200,199), length[1:])
-    plt.show()
+    # plt.plot(np.linspace(1,200,199), length[1:])
+    # plt.show()
     #
     # for i in range(len(markov[-1])-1):
     #     x1, y1 = markov[-1][i][1], markov[-1][i][2]
